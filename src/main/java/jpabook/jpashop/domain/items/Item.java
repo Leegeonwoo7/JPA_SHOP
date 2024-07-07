0eed0d33c866c2821b98fa18b656a7e18b25c7b5
Leegeonwoo7 <workgeon07@naver.com>
2024-07-08 03:00:12 +0900

    feat: 상품 비즈니스 로직 추가

--- src/main/java/jpabook/jpashop/domain/items/Item.java
+++ src/main/java/jpabook/jpashop/domain/items/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.items;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,4 +24,19 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /**
+     * 비즈니스 로직(DDD)
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
